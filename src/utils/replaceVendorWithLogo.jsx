be16d6e6b99197e19de41c86755b68efbab08ebb
bilazor4ik <bilazor@gmail.com>
2022-08-26 11:08:43 -0400

    Centered Logos

--- src/utils/replaceVendorWithLogo.jsx
+++ src/utils/replaceVendorWithLogo.jsx
@@ -4,16 +4,16 @@ import ups from '../assets/UPS-logo.png'
 import amazon from '../assets/Amazon-logo.png'
 export const replaceVendorWithLogo = (vendor) => {
     if (vendor.toLowerCase() === 'fedex') {
-        return <img src={fedex} alt={vendor}/>
+        return <img src={fedex} alt={vendor} className="mx-auto"/>
     }
     if(vendor.toLowerCase() === 'usps') {
-        return <img src={usps} alt={vendor}/>
+        return <img src={usps} alt={vendor} className="mx-auto"/>
     }
     if(vendor.toLowerCase() === 'ups') {
-        return <img src={ups} alt={vendor}/>
+        return <img src={ups} alt={vendor} className="mx-auto"/>
     }
     if(vendor.toLowerCase() === 'amazon') {
-        return <img src={amazon} alt={vendor}/>
+        return <img src={amazon} alt={vendor} className="mx-auto"/>
     }
     if(vendor.toLowerCase() === 'other') {
         return "Unknow"
