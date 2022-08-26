05970974c1ffed1b6842b573a5597f44c8ac577c
bilazor4ik <bilazor@gmail.com>
2022-08-26 08:30:36 -0400

    first version, functionality works

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import SharedLayout from "./components/SharedLayout";
+import AddDelivery from "./pages/AddDelivery";
+import DeliveryDetails from "./pages/DeliveryDetails";
+import ErrorPage from "./pages/ErrorPage";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="deliveries/:deliveryId" element={<DeliveryDetails />} />
+        <Route path="deliveries/new" element={<AddDelivery/>}/>
+      </Route>
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom';
+import AllDeliveries from '../components/AllDeliveries';
+
+
+
+export default function Home() {
+
+    
+    return (
+        <div className="px-4 sm:px-6 lg:px-8">
+            <div className="sm:flex sm:items-center">
+                <div className="sm:flex-auto">
+                    <h1 className="text-xl font-semibold text-gray-900">Deliveries</h1>
+                    <p className="mt-2 text-sm text-gray-700">
+                        A list of all deliveries for current week.
+                    </p>
+                </div>
+                <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+                    <Link to="deliveries/new">
+
+                        <button
+                            type="button"
+                            className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
+                        >
+                            Add delivery
+                        </button>
+                    </Link>
+                </div>
+            </div>
+          <AllDeliveries/>
+        </div>
+    )
+}
